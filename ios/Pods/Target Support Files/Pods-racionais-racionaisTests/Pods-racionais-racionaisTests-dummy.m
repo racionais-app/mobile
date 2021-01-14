2b36bf8f25fde6ff8772ea8f38b54062903f4f46
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-01-14 16:27:36 -0300

    chore: Ignore Pods

--- ios/Pods/Target Support Files/Pods-racionais-racionaisTests/Pods-racionais-racionaisTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_racionais_racionaisTests : NSObject
-@end
-@implementation PodsDummy_Pods_racionais_racionaisTests
-@end
--- ios/Pods/Target Support Files/Pods-racionais-tvOSTests/Pods-racionais-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_racionais_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_racionais_tvOSTests
-@end
