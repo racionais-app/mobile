f6e3ae6c1824525b4765054a35bde4e678a0079a
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2021-04-21 14:47:58 -0300

    Fraction Element

---
+++ app/screens/Question/Components/Fraction.js
@@ -0,0 +1,71 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+
+import Markdown from './Markdown';
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center'
+  },
+  textInput: {
+    marginTop: 8,
+    padding: 8,
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 4,
+    textAlign: 'center',
+    width: 44,
+    height: 44,
+    fontSize: 16
+  },
+  separator: {
+    width: 64,
+    height: 1,
+    marginTop: 8,
+    backgroundColor: 'black'
+  }
+});
+
+const Fraction = ({ element, onChange }) => {
+  const numerator = useRef();
+  const denominator = useRef();
+  const [data, setData] = useState({});
+
+  const onSubmit = () => {
+    denominator.current?.focus?.();
+  }
+
+  const onChangeText = (key, value) => {
+    setData(oldData => ({ ...oldData, [key]: value }));
+  }
+
+  useEffect(() => {
+    onChange(`${data.numerator ?? 1}/${data.denominator ?? 1}`);
+  }, [
+    data.numerator,
+    data.denominator
+  ]);
+
+  return (
+    <>
+      <Markdown>{element.label}</Markdown>
+      <View style={styles.container}>
+        <TextInput
+          ref={numerator}
+          style={styles.textInput}
+          onChangeText={text => onChangeText('numerator', text)}
+          onEndEditing={onSubmit}
+          onSubmitEditing={onSubmit}
+        />
+        <View style={styles.separator} />
+        <TextInput
+          ref={denominator}
+          style={styles.textInput}
+          onChangeText={text => onChangeText('denominator', text)}
+        />
+      </View>
+    </>
+  );
+}
+
+export default Fraction;
